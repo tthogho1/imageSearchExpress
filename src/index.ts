1c76ac16bd48f97f4c256e268e90752d4472e42f
tthogho1 <tthogho1@gmail.com>
2024-03-30 10:14:28 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,71 @@
+import express from 'express'
+import { Embedding } from './lib/embedding.ts';
+import { Pinecone , QueryResponse, FetchResponse ,RecordMetadata } from '@pinecone-database/pinecone';
+import type { webCamObj, webCamMetadata } from './lib/type.ts';
+import type { Photo } from './lib/type.ts';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app: express.Express = express()
+const pinecone = new Pinecone({
+    apiKey: process.env.PINECONE_API_KEY ??  '',
+});
+const index = pinecone.index(process.env.PINECONE_INDEX_NAME ?? '');
+const image_server = process.env.WINDY_IMAGE_SERVER ?? '';
+
+// CORSの許可
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
+
+app.use(express.static( __dirname + '/static'));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const embedding = new Embedding() 
+
+// GetとPostのルーティング
+const router: express.Router = express.Router()
+router.post('/api/searchWebcam', async (req:express.Request, res:express.Response) => {
+    const query = req.body.query as string;
+
+    const y = await embedding.getTextEmbedding(query);
+    const response = await index.namespace('webcamInfo').query({
+        topK: 5,
+        vector: y,
+        includeValues: false,
+        includeMetadata: true
+    });
+    
+    const { matches } = response;
+    const photos = matches.map(match => {
+        const metadata = match.metadata as webCamMetadata;
+        const photo : Photo = {
+            id: match.id,
+            score: match.score,
+            created_at: "",
+            width: 200,
+            height: 112,
+            description: metadata.title,
+            urls: {
+                small: image_server + match.id + ".jpg",
+            },
+            links: {
+                html: metadata.day,
+            }
+        }
+        return photo;
+    })
+    res.send(photos);
+})
+
+app.use(router)
+app.listen(3000,()=>{ console.log('Example app listening on port 3000!') })
---
+++ src/lib/embedding.ts
@@ -0,0 +1,32 @@
+import {AutoProcessor,AutoTokenizer,CLIPTextModelWithProjection,CLIPVisionModelWithProjection, PreTrainedTokenizer} from '@xenova/transformers';
+
+class Embedding {
+    private model: CLIPVisionModelWithProjection |null = null;
+    private tokenizer: PreTrainedTokenizer | null = null;
+    private textModel: CLIPTextModelWithProjection | null = null; 
+    private imageProcessor: AutoProcessor | null = null;
+
+    constructor() {
+        this.initializeModels();
+    }
+
+    private async initializeModels() {
+        const model_id = process.env.MODEL_ID;
+
+        this.model = await CLIPVisionModelWithProjection.from_pretrained(model_id as string);
+        this.tokenizer = await AutoTokenizer.from_pretrained(model_id as string);
+        this.textModel = await CLIPTextModelWithProjection.from_pretrained(model_id as string);
+        this.imageProcessor = await AutoProcessor.from_pretrained(model_id as string);
+    }
+
+    async getTextEmbedding(text: string): Promise<number[]> {
+        if (this.tokenizer === null || this.textModel === null) {
+            throw new Error('Model not initialized');
+        }
+        const textInputs = await this.tokenizer(text, { padding: true, truncation: true });
+        const { text_embeds } = await this.textModel(textInputs);
+        return Array.from(text_embeds.data);
+    }
+}
+
+export { Embedding };
